bf9b6aa9c629465d0fbf65d7d3e1101ef3b5eb4d
Polyakov Stepan <stepan@bubuka.info>
2023-07-24 14:26:08 +0700

    save

--- app/src/main/java/com/example/swapi/MainActivity.kt
+++ app/src/main/java/com/example/swapi/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.swapi
 import StarWarsApiClient
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.GridView
+import android.widget.Button
+import android.widget.Spinner
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 
 class MainActivity : AppCompatActivity() {
@@ -12,19 +15,32 @@ class MainActivity : AppCompatActivity() {
     private lateinit var apiClient: StarWarsApiClient
 
     private lateinit var searchTextView: TextView
+    private lateinit var startSearchButton: Button
+    private lateinit var spinner: Spinner
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         searchTextView = findViewById(R.id.searchTextView)
+        startSearchButton = findViewById(R.id.startSearchButton)
+        spinner = findViewById(R.id.spinner)
         apiClient = StarWarsApiClient()
-        // Отслеживайте изменения в тексте элемента ввода и выполняйте поиск при вводе более 2 символов
-//        searchTextView.addTextChangedListener { text ->
-//            if (text?.length ?: 0 >= 2) {
-//                searchCharacters(text.toString())
-//            }
-//        }
-    }
 
+        startSearchButton.setOnClickListener {
+            val selectedOption = spinner.selectedItem as String
+            val searchString = searchTextView.text.toString()
+            if(selectedOption == "персонажу"){
+                lifecycleScope.launch {
+                    val characterResponse = apiClient.searchCharacters(searchString)
+                    val character = characterResponse.results[0]
+
+                }
+            }
+            if(selectedOption == "звездолету"){
+
+            }
+        }
+    }
 }
